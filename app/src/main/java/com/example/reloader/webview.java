8226448efa7399ae3b1849936c35962dca8d65f1
mac <ghayoor10th@gmail.com>
2019-12-24 12:44:01 +0500

    AlertDialog box is set now to get time interval and duration

--- app/src/main/java/com/example/reloader/webview.java
+++ app/src/main/java/com/example/reloader/webview.java
@@ -4,14 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
 import android.util.Log;
 import android.view.View;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 
 public class webview extends Activity {
@@ -21,6 +28,10 @@ public class webview extends Activity {
     public Button btn_next;
     public Button btn_back;
     private ProgressDialog progDailog;
+    public AlertDialog.Builder alert;
+    public String time_interval;
+    public String time_duration;
+
 
 
     @SuppressLint({"NewApi"})
@@ -28,9 +39,10 @@ public class webview extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        alert = new AlertDialog.Builder(this);
+
         btn_next = (Button) findViewById(R.id.next);
         btn_back = findViewById(R.id.back);
-
 
         setContentView(R.layout.activity_webview);
 
@@ -65,11 +77,57 @@ public class webview extends Activity {
 
     }
 
+    public void dialog_box(String title, int flag){
+        Context context = this;
+        LinearLayout layout = new LinearLayout(context);
+        layout.setOrientation(LinearLayout.VERTICAL);
+
+        // Add a TextView here for the "Title" label, as noted in the comments
+        final EditText interval = new EditText(context);
+        interval.setHint("Interval");
+        layout.addView(interval); // Notice this is an add method
+
+        // Add another TextView here for the "Description" label
+        final EditText duration = new EditText(context);
+        duration.setHint("Duration");
+        layout.addView(duration); // Another add method
+
+        alert.setTitle("Set Values");
+        alert.setMessage("Enter Time Interval and Duration (sec)");
+
+        alert.setView(layout);
+
+
+        alert.setPositiveButton("NEXT", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+                //What ever you want to do with the value
+                time_interval = interval.getText().toString();
+                time_duration = duration.getText().toString();
+
+                if(time_interval.equals("") || time_duration.equals("")){
+                    dialog.dismiss();
+
+                    Toast toast=Toast. makeText(getApplicationContext(),"Please enter both values to get started",Toast. LENGTH_SHORT);
+                    toast.show();
+                }else{
+
+                }
+
+            }
+        });
+
+        alert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+                dialog.dismiss();
+            }
+        });
+
+        alert.show();
+    }
+
     public void next_btn(View view) {
 
-        Log.e("pass intent","here i'll pass intent");
-//        Intent i = new Intent(webview.this, config.class);
-//        i.putExtra("url", webView.getUrl());
-//        startActivity(i);
+        dialog_box("Select Time Duration", 0);
+        Log.w("000000000000000", "e check kro ny ");
     }
 }
